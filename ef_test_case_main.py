7a2d4e49e3066330acdde51d4db0d0d28c70bb54
tabou1984 <33222473+tabou1984@users.noreply.github.com>
2022-04-10 20:08:36 +0300

    Add files via upload
    The main executable file

---
+++ ef_test_case_main.py
@@ -0,0 +1,34 @@
+from ef_exercise_functions import *
+
+import logging
+import logging.handlers
+
+file_handler = logging.FileHandler(
+    './log_files/logs.log')
+file_format = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_format)
+
+file_logger = logging.getLogger('file_logger')
+file_logger.addHandler(file_handler)
+file_logger.setLevel(logging.DEBUG)
+
+date = str(input("Input date in yyyymmdd form: "))
+exercise = int(input("Input exercise number: "))
+
+def main(exercise, date):
+    if exercise == 1:
+        print(exercice_one(date))
+    if exercise == 2:
+        print(exercice_two(date))
+
+        
+if __name__ == "__main__":
+
+    try:
+        file_logger.info("Function Started")
+        main(exercise, date)
+        file_logger.info("Function Finished Successfully")
+    except Exception as e:
+        file_logger.exception(e)
+        raise(e)
